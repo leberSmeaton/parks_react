cae7ef496b52150741f94ca775424c7827ddce79
leberSmeaton <77179232+leberSmeaton@users.noreply.github.com>
2021-12-13 15:54:36 +1100

    BELLA - updating dummy data for posts.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { getParkPosts } from './services/parkPostServices';
 
 const App = () => {
+  const [parkPosts, setParkPosts] = useState([]);
+  // first things first, load
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    getParkPosts()
+      .then(posts => {
+        console.log(posts)
+        setParkPosts(posts)
+      })
+      .catch(err => console.log(err))
+      .finally(() => setLoading(false))
+  }, [])
+
   return (
     <div >
-          The basic empty template
+      The basic empty template
+      {
+      loading 
+      ?
+      (<p>Loading</p>)
+      :
+      (<p>Got Posts!</p>)
+      }
     </div>
   )
 }
