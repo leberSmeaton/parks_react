3ec0a1cbfa41bccdcc5bb4604e6ed1790f6c2108
“Karla <karlamariztolentino@gmail.com>
2021-12-15 11:29:20 +1100

    removed lorem ipsum in app.js

--- src/App.js
+++ src/App.js
@@ -29,9 +29,6 @@ const App = () => {
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
       <GlobalStyle />
       <BrowserRouter>
         <NavBar />
