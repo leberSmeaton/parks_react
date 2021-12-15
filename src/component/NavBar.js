c67697691d46d3ab967f4f94be4625676e13dfd6
“Karla <karlamariztolentino@gmail.com>
2021-12-15 11:59:21 +1100

    created hamburger menu for mobile. styled navbar component

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -3,15 +3,20 @@ import { NavLink } from 'react-router-dom';
 
 export default function NavBar() {
   return (
-    <div>
-      <ul>
-        <li><NavLink to="/">Picnic Victoria</NavLink></li>
-        <li><NavLink to="/">Map</NavLink></li>
-        <li><NavLink to="/list">List</NavLink></li>
-        <li><NavLink to="/about">About</NavLink></li>
-        <li><NavLink to="/signin">Sign In</NavLink></li>  
-      </ul>
+    <nav className='flex justify-between items-center h-16 bg-white text-black relative shadow-sm font-mono' role='navigation'>
+      <NavLink className="pl-8" to="/">Picnic Victoria</NavLink>
+      <div className="px-4 cursor-pointer md:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+        </svg>
+      </div>
+      <div className="pr-8 md:block hidden">
+        <NavLink className="p-4" to="/">Map</NavLink>
+        <NavLink className="p-4" to="/list">List</NavLink>
+        <NavLink className="p-4" to="/about">About</NavLink>
+        <NavLink className="p-4" to="/signin">Sign In</NavLink>
+      </div>
       
-    </div>
+    </nav>
   )
 }
