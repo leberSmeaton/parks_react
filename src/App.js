775022cf6eaf35f096a2953612ae43c05e61e652
leberSmeaton <77179232+leberSmeaton@users.noreply.github.com>
2021-12-15 14:51:49 +1100

    Adding global state and dummy parks data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { getParkPosts } from './services/parkPostServices';
+import {getParks} from './services/parkServices';
 import { GlobalStyle } from './styled-components/globalStyles';
 import { ListView } from './component/ListView';
 import NavBar from './component/NavBar';
@@ -9,36 +10,79 @@ import MapView from './component/MapView';
 import About from './component/About';
 import SignIn from './component/SignIn';
 import SignUp from './component/SignUp';
+import reducer from './utils/reducer';
 
 const App = () => {
-  const [parkPosts, setParkPosts] = useState([]);
-  // first things first, load
-  const [loading, setLoading] = useState(true);
-  
+  // const [parkPosts, setParkPosts] = useState([]);
+  // const [loading, setLoading] = useState(true);
+
+  const initialState = {
+    parkPosts: [],
+    loading: true,
+    parks: []
+  }
+
+  const [store, dispatch] = useReducer(reducer, initialState);
+  const {parkPosts} = store;
+  const {loading} = store;
+  const {parks} = store;
+
+  const ParksContext = React.createContext()
+
+  // Posts data from parkPostServices.js
   useEffect(() => {
     getParkPosts()
       .then(posts => {
         console.log(posts)
-        setParkPosts(posts)
+        dispatch({
+          type: 'setParkPosts',
+          data: posts
+        })
       })
       .catch(err => console.log(err))
-      .finally(() => setLoading(false))
+      .finally(() => 
+        dispatch({
+          type: 'setLoading',
+          data: false
+        })  
+      )
+  }, [])
+
+  // Parks data from parkServices.js
+  useEffect(() => {
+    getParks()
+      .then(parks => {
+        console.log(parks)
+        dispatch({
+          type: 'setParks',
+          data: parks
+        })
+      })
+      .catch(err => console.log(err))
+      .finally(() => 
+        dispatch({
+          type: 'setLoading',
+          data: false
+        })  
+      )
   }, [])
 
   return (
     <>
-      <GlobalStyle />
-      <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<MapView />}></Route>
-          <Route path="/list" element={<ListView loading={loading} posts={parkPosts} />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/signin" element={<SignIn />}></Route>
-          <Route path="/signup" element={<SignUp />}></Route>
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <ParksContext.Provider value={parks}>
+        <GlobalStyle />
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<MapView />}></Route>
+            <Route path="/list" element={<ListView loading={loading} posts={parkPosts} parks={parks} />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/signin" element={<SignIn />}></Route>
+            <Route path="/signup" element={<SignUp />}></Route>
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </ParksContext.Provider>
     </>
   )
 }
--- src/component/Park.js
+++ src/component/Park.js
@@ -1,14 +1,24 @@
 import React from 'react'
+// import React, {useReducer} from 'react'
+// import reducer from './utils/reducer';
+// import parks from '../data/parks';
 
-export default function Park() {
+export default function Park(props) {
+  // const [store, dispatch] = useReducer(reducer, initialState);
+  const {parks} = store;
+
   return (
     <>
       <ul>
         <li>
           <p>🌴</p>
+          <p>icon: {parks.park_icon}</p>
           <h4>All Nations Park</h4>
+          <p>name: {parks.park_name}</p>
           <p>Separation St, Northcote VIC 3070</p>
+          <p>address: {parks.address_id}</p>
           <p>picnic, food nearby, nature, skatepark, playground</p>
+          <p>feature: {parks.feature_id}</p>
           <p>⭐️  ⭐️  ⭐️  ⭐️ </p>
         </li>
       </ul>
