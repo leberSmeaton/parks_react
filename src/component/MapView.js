bdea42fb4927bbb30e73901d8a9d1e8bd5c466e0
leberSmeaton <77179232+leberSmeaton@users.noreply.github.com>
2022-01-10 11:31:37 +1100

    ADDED MAP VIEW with Google npm api

--- src/component/MapView.js
+++ src/component/MapView.js
@@ -1,13 +1,60 @@
 import React from 'react'
+import {
+  GoogleMap,
+  useJsApiLoader
+} from "@react-google-maps/api";
+import mapStyles from '../mapStyles';
+
+const containerStyle = {
+  width: '100vw',
+  height: '90vh'
+};
+
+const center = {
+  lat: -37.840,
+  lng: 144.946
+};
+
+const options = {
+  styles: mapStyles
+}
 
 export default function MapView() {
-  const category = [ "all", "community", "child friendly", "dog park", "large park", "nature", "skatepark", "sports"]
-  return (
-    <div>
-      <div>
-        {category.map(cat => cat)}
-      </div>
-      <img src={process.env.PUBLIC_URL + '/large_melbourne.png'} alt="melbourne google map" style={{ width: '100%' }} />
-    </div>
-  )
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    // googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+    googleMapsApiKey: "AIzaSyC8NxtPpxOWXgvnjId9HRzz-hG9Wlcj6AA"
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={11}
+      options={options}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      { /* Child components, such as markers, info windows, etc. */ }
+      <>
+      </>
+    </GoogleMap>
+  ) 
+    : ( 
+    <>
+      <p>Not Loaded</p>
+    </>
+    )
 }
