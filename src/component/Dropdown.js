b5be0958352278530d94ab6037e024bae6dfe1a0
leberSmeaton <77179232+leberSmeaton@users.noreply.github.com>
2022-01-13 12:59:52 +1100

    Updated route for auth/signing

--- src/component/Dropdown.js
+++ src/component/Dropdown.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { useGlobalState } from '../utils/stateContext';
 
 const Dropdown = ({isOpen, toggle}) => {
-    return (
-        <div className={isOpen? 'dropdown': 'hidden'}>
-          <NavLink className="p-4" to="/">Map</NavLink>
-          <NavLink className="p-4" to="/parks">List</NavLink>
-          <NavLink className="p-4" to="/about">About</NavLink>
-          <NavLink className="p-4" to="/signin">Sign In</NavLink>
-        </div>
-    )
+  const {store} = useGlobalState();
+  const {signedInUser} = store;
+  
+  return (
+    <div className={isOpen? 'dropdown': 'hidden'}>
+      <NavLink className="p-4" to="/">Map</NavLink>
+      <NavLink className="p-4" to="/parks">List</NavLink>
+      <NavLink className="p-4" to="/about">About</NavLink>
+      {/* <NavLink className="p-4" to="/auth/signin">Sign In</NavLink> */}
+      {signedInUser ?
+        (<NavLink className="p-4" to="/">Sign Out</NavLink>)
+      :
+        (<NavLink className="p-4" to="/auth/signin">Sign In</NavLink>)
+      }
+      <span>| Hello {signedInUser || "guest"}</span>
+    </div>
+  )
 }
 
 export default Dropdown
