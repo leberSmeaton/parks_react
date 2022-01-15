71fe5fdce90530ce7f4138cef3cbd648c5071da4
“Karla <karlamariztolentino@gmail.com>
2022-01-15 15:22:38 +1100

    added div classes to about.js. Made changes to style.css for padding containers

--- src/component/About.js
+++ src/component/About.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
+
 export default function About() {
   return (
     <>
       <section>
         <h2>About Picnic Vic</h2>
-        <div>
-          <img src={process.env.PUBLIC_URL + '/park_image1.jpg'} alt="melbourne google map" style={{ width: '100%' }} />
-          <p>
+        <div className='about-container'>
+          <img className='park-img' src={process.env.PUBLIC_URL + '/park_image1.jpg'} alt="melbourne google map" />
+          <p className='about-park'>
             In a post Covid world we now know more than ever that we need to connect with our friends and family, outdoors in the real world. We need to utilise our green open spaces to enrich our lives.
             To meet calls and pressure that local councils and the State Government aren't providing enough green space, Parks Victoria are creating a new inner city park database. For small and large parks alike!
             The Picnic Victoria app provides detailed park amenity information to help facilitate a family picnic to a skate park hangout.
@@ -16,10 +17,10 @@ export default function About() {
       </section>
       <section>
         <h2>Our Wine + Cheese Partners</h2>
-        <div>
-          <img src={process.env.PUBLIC_URL + '/picnic_image2.jpg'} alt="melbourne google map" style={{ width: '100%' }} />
+        <div className='partners-container'>
+          <img className='cheese-wine-img' src={process.env.PUBLIC_URL + '/picnic_image2.jpg'} alt="melbourne google map" />
           <strong>A park is nothing without a picnic and nothing is better than a picnic in Vic!</strong>
-          <p>
+          <p className='about-partners'>
             Want to reach peak picnic? 🍷 + 🧀 = 🧺
             The Picnic Victoria app have teamed up with Victorian Wine and Cheese producers to pair your local park with local picnic produce. The main focus of these partnerships is to promote (and support) local boutique producers and bring their products from all corners of Victoria (and beyond).
             <strong>It doesn't get much Gouda than that.</strong>
