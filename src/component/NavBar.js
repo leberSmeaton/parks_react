5046145944a9a9d9a51ca0d205e998e1a17d1491
“Karla <karlamariztolentino@gmail.com>
2022-01-16 11:13:36 +1100

    added font family to index.html. Changed padding in css for SignIn.js

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -13,7 +13,7 @@ export default function NavBar({toggle}) {
 
   return (
     <nav className='nav-bar' role='navigation'>
-      <NavLink className="pl-8" to="/">Picnic Victoria</NavLink>
+      <NavLink className="pl-8 picnic-vic" to="/">Picnic Victoria</NavLink>
       <div className="px-4 cursor-pointer md:hidden" onClick={toggle}>
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
