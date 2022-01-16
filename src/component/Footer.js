1d457f9449cd4b527482bcab87e62cc804fedbce
“Karla <karlamariztolentino@gmail.com>
2022-01-16 12:28:06 +1100

    changed footer text, added github iconand removed extra text

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -4,25 +4,10 @@ import {Link} from 'react-router-dom';
 export default function Footer() {
   return (
     <div className="footer">
-      <Link to='www.facebook.com'>Facebook</Link>
-      <Link to='www.medium.com'>Medium</Link>
-      <Link to='www.twitter.com'>Twitter</Link>
-      <Link to='https://github.com/PicnicVic'>Git Repo</Link>
-      Team on LinkedIn: <Link to="/">Karla T</Link>, <Link to="/">Matt L</Link>, <Link to="/">Ruilai Z</Link>, <Link to='https://www.linkedin.com/in/bella-leber-smeaton/'>Bella LS</Link>
+      
+      <Link to='https://github.com/PicnicVic'><img className='github-img' src={process.env.PUBLIC_URL + '/github-icon.png'} alt="melbourne google map" /></Link>
+      {/* Team on LinkedIn: <Link to="/">Karla T</Link>, <Link to="/">Matt L</Link>, <Link to="/">Ruilai Z</Link>, <Link to='https://www.linkedin.com/in/bella-leber-smeaton/'>Bella LS</Link> */}
       <p>Copyright 2021 Picnic Victoria All rights reserved.</p>
     </div>
-
-    // <div>
-    //   <ul>
-    //     <li></li>
-    //     <li></li>
-    //     <li></li>
-    //   </ul>
-      
-    //   <ul>
-    //     <li></li>
-    //     <li></li>
-    //   </ul>
-    // </div>
   )
 }
